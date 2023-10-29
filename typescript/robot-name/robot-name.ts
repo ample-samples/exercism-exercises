e3c048b880bc8de200650172975a1be653555144
Todd Griffin <todd.griffin61@outlook.com>
2023-10-29 15:03:08 +0000

    finished robot-name

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,15 +1,42 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const robotNames: string[] = []
+
+function generateName(): string {
+  let newName = ""
+  let i = 1
+  while (i<=2) {
+    newName +=  characters.charAt(Math.floor(Math.random() * characters.length))
+    i++
+  }
+  newName += Math.floor(Math.random() * 1000).toString().padStart(3, "0");
+
+  if (robotNames.includes(newName)) {
+    newName = generateName()
+  }
+
+  robotNames.push(newName)
+  return newName
+}
+
+generateName()
+
 export class Robot {
-  constructor() {}
+  robotName: string;
+
+  constructor() {
+   this.robotName = generateName(); 
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this.robotName
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    this.robotName = generateName(); 
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    robotNames.length = 0
   }
 }
