331f78031ebda1871ee302f9151eed8a2a0063d9
Todd Griffin <todd.griffin61@outlook.com>
2023-10-30 20:57:02 +0000

    modified robot-name

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -2,23 +2,17 @@ const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const robotNames: Set<string> = new Set([]);
 
-const NAME_RE = /^[A-Z]{2}\d{3}$/
-
-
-// create name
 function createName(): string {
   let newName = "";
-  let i = 1;
-  while (i<=2) {
-    newName +=  characters.charAt(Math.floor(Math.random() * characters.length));
-    i++;
+  for (let i=0; i<2; i++) {
+    newName +=  characters.charAt(Math.floor(Math.random() * 26));
   }
   newName += Math.floor(Math.random() * 1000).toString().padStart(3, "0");
 
   return newName;
 }
-// add name to robot and name set
- function robotName(): string {
+
+function robotName(): string {
   let newName = createName();
 
   while (robotNames.has(newName)) {
@@ -27,9 +21,9 @@ function createName(): string {
 
   robotNames.add(newName);
   return newName;
- }
+}
 
-export class Robot {
+class Robot {
   robotName: string;
 
   constructor() {
@@ -49,29 +43,24 @@ export class Robot {
   }
 }
 
-// let robot: Robot;
-// robot = new Robot();
-//
-// const TOTAL_NUMBER_OF_NAMES =
-//   26 * // A-Z
-//   26 * // A-Z
-//   10 * // 0-9
-//   10 * // 0-9
-//   10; // 0-9
-//
-// const usedNames = new Set();
-// usedNames.add(robot.name);
-//
-// for (let i = 0; i < TOTAL_NUMBER_OF_NAMES - 1; i += 1) {
-//   const sizeBefore = usedNames.size
-//   const newRobot = new Robot()
-//   usedNames.add(newRobot.name)
-//   const sizeAfter = usedNames.size
-//   if (sizeAfter === sizeBefore) {
-//     console.log(`Weird name: ${newRobot.name} after ${usedNames.size} name generated. Is in set already? ${robotNames.has(newRobot.name)}`)
-//   }
-//   // console.log(usedNames.size, `Loop ittr ${i}`)
-// }
-//
-// console.log(`You created ${usedNames.size}/${TOTAL_NUMBER_OF_NAMES} names.`)
+let startTime = performance.now();
 
+const TOTAL_NUMBER_OF_NAMES =
+  26 * // A-Z
+  26 * // A-Z
+  10 * // 0-9
+  10 * // 0-9
+  10; // 0-9
+
+const usedNames = new Set();
+
+for (let i = 0; i < TOTAL_NUMBER_OF_NAMES; i += 1) {
+  const newRobot = new Robot()
+  usedNames.add(newRobot.name)
+}
+
+let endTime = performance.now();
+let timeElapsed = endTime - startTime;
+console.log(`That took ${Math.floor(timeElapsed)}ms`)
+
+console.log(`You created ${usedNames.size}/${TOTAL_NUMBER_OF_NAMES} names.`)
