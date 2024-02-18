a271ecf92677b0d9bf2b2a7ae56c77d6881e5e5f
Todd <todd.griffin61@outlook.com>
2024-02-18 22:03:09 +0000

    completed hamming

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,6 +1,16 @@
 local Hamming = {}
 
 function Hamming.compute(a, b)
+  if #a ~= #b then return -1 end
+
+  local hamming_dist = 0;
+  for i = 1, #a do
+    if a:sub(i, i) ~= b:sub(i, i) then
+      hamming_dist = hamming_dist + 1
+    end
+  end
+
+  return hamming_dist
 end
 
 return Hamming
